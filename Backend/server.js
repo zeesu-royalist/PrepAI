8dc5b1a7b0e5e167322c6baa542c4e1ac4a445b4
Zeesu Royalist <rzeesu@gmail.com>
2026-07-12 01:21:06 +0530

    fix last

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,12 @@
-require("dotenv").config()
-const app = require("./src/app")
-const connectToDB = require("./src/config/database")
+require("dotenv").config();
 
-connectToDB()
+const app = require("./src/app");
+const connectToDB = require("./src/config/database");
 
+connectToDB();
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000")
-})
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
