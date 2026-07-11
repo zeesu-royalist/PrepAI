1bd9206598f940b7734d2982f9027be7b31c2735
Zeesu Royalist <rzeesu@gmail.com>
2026-07-12 01:49:57 +0530

    api fix

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://prepai-backend-qwt3.onrender.com",
     withCredentials: true,
 })
 
